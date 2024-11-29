66003aa42298c6a94cbd790ad086a1bc10690fe8
Tushar Kanti Das <tusharkanti647@gmail.com>
2024-11-29 21:28:50 +0530

    changes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://quiz-app-tushar.web.app'],
     credentials: true,
 }
 app.use(cors(corsOptions));
