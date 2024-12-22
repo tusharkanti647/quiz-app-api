9e444e32788cf244c4b6ace3076a1228afe0c20b
Tushar Kanti Das <tusharkanti647@gmail.com>
2024-12-22 20:37:24 +0530

    add commend

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 const isAuthenticated = async (req, res, next) => {
     try {
         const token = req.cookies.JWTToken;
-
+        console.log("authencat token", token)
         if (!token) {
             return res.status(401).json({ message: 'User not Authenticated', success: false });
         }
